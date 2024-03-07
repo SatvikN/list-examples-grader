3086dabe74a27031d5b4b26bce480162d2ec2845
hamzamohiuddin1 <66460751+hamzamohiuddin1@users.noreply.github.com>
2024-03-07 12:45:04 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -7,10 +7,31 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
-
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [ -f "student-submission/ListExamples.java" ]; then
+    echo "File found"
+else
+    echo "ListExamples.java was not found"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]; then
+    echo "Compilation error"
+    exit 1
+fi
+
+echo "Program compiled successfully"
+
+$CPATH org.junit.runner.JUnitCore TestListExamples
